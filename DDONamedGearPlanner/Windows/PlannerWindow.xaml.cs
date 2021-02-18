ca70d86bafcf179a16d3c3103e2500e032702b2a
Martin Actor <pfhoenix@gmail.com>
2021-02-18 10:57:35 -0500

    Updated version number

--- DDONamedGearPlanner/Windows/PlannerWindow.xaml.cs
+++ DDONamedGearPlanner/Windows/PlannerWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace DDONamedGearPlanner
 	/// </summary>
 	public partial class PlannerWindow : Window
 	{
-		public static readonly string VERSION = "0.8.6";
+		public static readonly string VERSION = "0.8.7";
 
 		public GearSetBuild CurrentBuild = new GearSetBuild();
 
